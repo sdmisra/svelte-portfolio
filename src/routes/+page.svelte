80b9e66b29be32f4a1b27055918d68019de56303
Shane Misra <shane.misra@gmail.com>
2023-08-02 20:12:10 -0600

    begin layout of welcome page, basic animations on button hover

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,22 +5,24 @@
   import AboutMe from './aboutme.svelte'
   import Welcome from './welcome.svelte'
 
-  let projects = {
+  let deployedProjects = {
     'Avian Anthems' : 'https://avian-anthems.vercel.app/',
     'Mars Chronicles' : 'https://mars-chronicles.vercel.app/',
     'Hyper Loom' : 'https://hyper-loom-explorer.vercel.app/'
   }
-export let url ='';
+
+
+  export let url ='';
 </script>
 
 <Router {url}>
   <header class='portfolio-header'>
     <h1 class='header-text'>Welcome to my Professional Portfolio!</h1>
-    <Link to='/'>Home</Link>
     <nav class='header-link-container'>
-      <Link to='/projects'> Projects </Link>
-      <Link to='/about'> About Me </Link>
-      <Link to='/blog' >Blog </Link>
+      <Link to='/' class='header-link'>Home</Link>
+      <Link to='/projects' class='header-link'> Projects </Link>
+      <Link to='/about' class='header-link'> About Me </Link>
+      <Link to='/blog' class='header-link'>Blog </Link>
     </nav>
   </header>
   <Route exact path='/'>
@@ -37,36 +39,55 @@ export let url ='';
   </Route>
   </Router>
   <footer class='footer-link-container'>
-    <p class='footer-text'>Direct Project Links:</p>
-    <a href={projects['Avian Anthems']} class='footer-link'>Avian Anthems</a>
-    <a href={projects['Mars Chronicles']} class='footer-link'>Mars Chronicles</a>
-    <a href={projects['Hyper Loom']} class='footer-link'>Hyper Loom</a>
+    <p class='footer-text'>A few deployed projects:</p>
+    <a href={deployedProjects['Avian Anthems']} class='footer-link'>Avian Anthems</a>
+    <a href={deployedProjects['Mars Chronicles']} class='footer-link'>Mars Chronicles</a>
+    <a href={deployedProjects['Hyper Loom']} class='footer-link'>Hyper Loom</a>
   </footer>
 
 <style>
+
+  :global(body) {
+      width: 100vw;
+      height: 100vh;
+      background: aliceblue;
+      background-image: url('$lib/assets/MirroredTundra.png');
+      background-size: cover;
+    }
 
   h1 {
     display:flex;
     justify-content: center;
     font-size: 4rem;
-    background: radial-gradient(circle, rgba(232,208,65,0.94) 5%, rgba(227,187,80,0.91) 25%, rgba(186,27,194,1) 100%);
+    background: rgba(240, 248, 255, 0.426);
     border-radius: .75rem;
   }
+
   .header-link-container {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    justify-content: space-evenly;
   }
-
-  p {
+  
+  .footer-text {
     display:flex;
     justify-content: center;
     font-size: 2rem;
   }
-
+  
   footer {
     display: flex;
     align-items: center;
     flex-direction: column;
     font-size: 1.5rem;
   }
+  
+    :global(a) {
+      text-decoration: none;
+      color: aliceblue;
+    }
+  
+    :global(a):hover {
+      color: rebeccapurple;
+    }
 </style>
--- src/routes/welcome.svelte
+++ src/routes/welcome.svelte
@@ -1,40 +1,90 @@
 <script>
   import Resume from '$lib/assets/ShaneMisraATS.pdf'
 	import Pic from '$lib/assets/Shane_Profile.jpg'
-  import Background from '$lib/assets/loom_background.jpg'
 </script>
 
 <main class='welcome-body'>
+  <section class='left-panel'>
+
+  </section>
   <p class='welcome-text'>
     This page is a work in progress, but this is the perfect place to start if you are looking to see what I am capable of as a software developer thus far!
   </p>
-  <section class='welcome-picture'>
-    <img src={Pic} alt="the website creator, Shane Misra"/>
-    <img src={Background} alt='background'/>
-  </section>
-  <a href={Resume} class='direct-link'>View my Resume</a>
-  <button class='download-button'>
-    <a href={Resume} class='direct-link' download>
-      Download my Resume
-    </a>
-  </button>
+  <div class='resume-container'>
+    <section class='welcome-picture'>
+      <img src={Pic} alt="the website creator, Shane Misra"/>
+    </section>
+    <button class='resume-button view-button'>
+      <a href={Resume} class='direct-link'>View my Resume</a>
+    </button>
+    <button class='resume-button download-button'>
+      <a href={Resume} class='direct-link' download>Download my Resume</a>
+    </button>
+  </div>
   </main>
 
   <style>
+  
+  .welcome-body {
+    display:flex;
+    background: rgba(240, 248, 255, 0.426);
+  }
 
   p {
     display:flex;
     justify-content: center;
     font-size: 2rem;
   }
+  .resume-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: .1rem;
+    padding: .15rem;
+    border: 1px solid black;
+  }
   .welcome-picture {
     display: flex;
     justify-content: center;
+  }
+  .resume-button {
+    scale: 1.1;
+    border-radius: .25rem;
+    background-color: rgba(102, 51, 153, 0.535);
+  }
+
+  .resume-button:hover {
+    animation: grow-and-shrink infinite 2s;
+  }
+
+  .direct-link {
+    border: 1px solid black;
   }
   img {
     max-height: 60vh;
     max-width: 60vw;
     border-radius: 1rem;
     border: 1px solid black;
+  }
+
+  @keyframes grow-and-shrink {
+    0% {
+      scale: 1.1;
+    }
+    20% {
+      scale: 1.13;
+    }
+    40% {
+      scale: 1.15;
+    }
+    60% {
+      scale: 1.15;
+    }
+    80% {
+      scale: 1.13;
+    }
+    100% {
+      scale: 1.1
+    }
   }
   </style>
