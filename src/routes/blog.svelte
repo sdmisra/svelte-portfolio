e2babddfd89f27b6c431790e1046636419b51a98
Shane Misra <shane.misra@gmail.com>
2023-08-03 10:09:35 -0600

    api calls written, blog begun

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -1,15 +1,62 @@
 <script>
-  let words = 'hello world'
+  import {onMount} from 'svelte'
+  import {getHeadlines, getSearch} from '$lib/apiCalls.js'
+  let headlineStories = []
+  let resultsStories = []
+  let searchText = ''
+
+  onMount(async ()=> {
+    let headlines =  await getHeadlines()
+    headlineStories = headlines.articles
+  })
+
+  const searchFunction = async (query)=> {
+    let results = await getSearch(query)
+    resultsStories = results.articles
+  }
+
 </script>
 
 <main class='blog-body'>
-  <div class='blog-segment'>
-    This is an entry in the blog!
+  <div class="search-bar">
+    <input type='text' class="input-bar" placeholder="Search for a story..." on:change={event => searchText = event.target.value} 
+    value={searchText}/>
+    <button class="search-button" 
+    on:click={searchFunction(searchText) }>Search</button>
   </div>
+  <section class="headlines">
+    {#each headlineStories as story}
+      <figure class='story-card'>
+        <img src={story.urlToImage} alt={story.title} class='thumbnail'/>
+        <figcaption>{story.title}</figcaption>
+      </figure>
+    {:else}
+      <p class='loading-text'>Loading Articles!</p>
+    {/each}
+  </section>
 </main>
 
 <style>
+
+  .loading-text {
+    font-family: 'Menzanine', sans-serif;
+    color: aliceblue;
+    text-shadow: -1px 1px 2px #000,
+				  1px 1px 2px #000,
+				  1px -1px 0 #000,
+				  -1px -1px 0 #000;
+  }
+  .story-card {
+    border: 1px black solid;
+  }
+  .thumbnail {
+    max-width: 15%;
+    max-height: 10%;
+  }
   .blog-body {
+    display:flex;
+    flex-direction: column;
+    align-items: center;
     min-height: 60vh;
     min-width: 100%;
     background: rgba(240, 248, 255, 0.522);
